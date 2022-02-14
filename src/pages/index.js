9d5d39baba1139249ad108935ee7da056679bb86
nineey <56111891+nineey@users.noreply.github.com>
2022-02-14 21:03:44 +0100

    auth test

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 import { Spinner } from "@chakra-ui/react";
 
 export default function Home() {
-  const { data: status, session } = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
 
   const [dealUrl, setUrl] = useState("https://daydeal.ch/");
@@ -30,10 +30,6 @@ export default function Home() {
         />
       </div>
     );
-  }
-
-  if (status === "unauthenticated" || data === "undefined" || data === "null") {
-    return router.push("/api/auth/signin");
   }
 
   if (status === "authenticated") {
@@ -123,4 +119,6 @@ export default function Home() {
       </Layout>
     );
   }
+
+  return router.push("/api/auth/signin");
 }
