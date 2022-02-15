61df5d1d488513c08e7c70ea893af57b49239275
nineey <56111891+nineey@users.noreply.github.com>
2022-02-15 13:10:37 +0100

    adjust time format UTC+1

--- src/pages/api/getback.js
+++ src/pages/api/getback.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import moment from "moment";
 import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
@@ -29,15 +30,24 @@ export default async function handler(req, res) {
         dealType,
       } = req.body;
       const date = new Date(sendDate);
-      const timeOffset = date.getTimezoneOffset();
 
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, "0");
-      const day = String(date.getDate()).padStart(2, "0");
-      const hours = String(date.getHours()).padStart(2, "0");
-      const minutes = String(date.getMinutes()).padStart(2, "0");
+      console.log(
+        moment.parseZone(date).local(true).format("YYYY-MM-DD HH:mm")
+      );
 
-      const send_at = `${year}-${month}-${day} ${hours}:${minutes}`;
+      // const year = date.getFullYear();
+      // const month = String(date.getMonth() + 1).padStart(2, "0");
+      // const day = String(date.getDate()).padStart(2, "0");
+      // const hours = String(date.getHours()).padStart(2, "0");
+      // const minutes = String(date.getMinutes()).padStart(2, "0");
+
+      // const send_at = `${year}-${month}-${day} ${hours}:${minutes}`;
+
+      const send_at = moment
+        .parseZone(date)
+        .local(true)
+        .format("YYYY-MM-DD HH:mm");
+
       const template_id =
         dealType === "special"
           ? process.env.GETBACK_TEMPLATE_ID_SPECIALS
@@ -70,23 +80,23 @@ export default async function handler(req, res) {
       // console.log(body);
 
       //testing
-      res.status(200).json({ message_id: 123456789 });
+      // res.status(200).json({ message_id: 123456789 });
 
       // !!! only uncomment this when you want to trigger real messages !!!
-      // try {
-      //   const data = (
-      //     await axios.post("https://api.getback.ch/v1/push/message", body, {
-      //       headers: {
-      //         Authorization: process.env.GETBACK_API_KEY,
-      //         "Content-Type": "application/json",
-      //       },
-      //     })
-      //   ).data;
-      //   console.log(data);
-      //   res.status(200).json(data);
-      // } catch {
-      //   res.status(500).send("Getback API error");
-      // }
+      try {
+        const data = (
+          await axios.post("https://api.getback.ch/v1/push/message", body, {
+            headers: {
+              Authorization: process.env.GETBACK_API_KEY,
+              "Content-Type": "application/json",
+            },
+          })
+        ).data;
+        console.log(data);
+        res.status(200).json(data);
+      } catch {
+        res.status(500).send("Getback API error");
+      }
     }
   } else {
     res.status(401).json("No access!");
