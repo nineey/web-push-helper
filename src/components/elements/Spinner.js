867572ed0f77d7b181ecb6210dd557eb57ba99e9
nineey <56111891+nineey@users.noreply.github.com>
2022-02-25 20:20:16 +0100

    remove chakra test 2

--- src/components/elements/Spinner.js
+++ src/components/elements/Spinner.js
@@ -1,15 +1,16 @@
-import { Spinner } from "@chakra-ui/react";
+// import { Spinner } from "@chakra-ui/react";
 
 export default function FullScreenSpinner() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Spinner
+      {/* <Spinner
         thickness="4px"
         speed="0.65s"
         emptyColor="gray.200"
         color="green.500"
         size="xl"
-      />
+      /> */}
+      Loading...
     </div>
   );
 }
